14b09b99a8089a145b4bd70442a56f87029d23d3
AminaB <aminatou.barry97@gmail.com>
2025-02-27 17:56:58 +0100

    Singleton using instantiation or static bloc

--- singleton/DateUtil.java
+++ singleton/DateUtil.java
@@ -11,3 +11,26 @@ public class DateUtil {
         return  instance;
     }
 }
+
+// using instantiation
+class DateUtil2 {
+    private static  DateUtil2 instance= new DateUtil2();
+    private DateUtil2(){
+    }
+    public static DateUtil2 getInstance(){
+        return  instance;
+    }
+}
+
+// using static bloc
+class DateUtil3 {
+    private static  DateUtil3 instance;
+    static {
+        instance= new DateUtil3();
+    }
+    private DateUtil3(){
+    }
+    public static DateUtil3 getInstance(){
+        return  instance;
+    }
+}
